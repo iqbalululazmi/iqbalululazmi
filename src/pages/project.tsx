74b01291b2a1a07a31f68d3e110c33a7eb87ab9a
iqbalululazmi <iqbalululazmi@gmail.com>
2021-08-25 21:45:37 +0700

    feat: add esdm and studi

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -20,8 +20,8 @@ export default function Project() {
             <div className="text-center p-16">
               <h1 className="text-white-pure text-6xl font-bold">Awesome Projects</h1>
             </div>
-            <div className="w-full h-full py-3 rounded-tl-100 rounded-tr-100 bg-project-1">
-              <div className="my-10 ml-16">
+            <div className="w-full h-full pb-10 py-3 rounded-tl-100 rounded-tr-100 bg-project-1">
+              <div className="my-16 ml-16">
                 <h1 className="text-5xl font-medium">Greatday Benefit</h1>
               </div>
               <div className="flex flex-row w-full justify-center">
@@ -59,11 +59,86 @@ export default function Project() {
                 </div>
               </div>
             </div>
-            {/* <div className="w-full h-full rounded-tl-100 rounded-tr-100 bg-project-2 flex flex-row justify-between">
-              <div className="mt-16 ml-16  w-32rem">
-                <h1 className="text-5xl font-semibold">FEB UMJ E-SDM</h1>
+          </div>
+        </section>
+        <section id="portofolio" className="h-full bg-project-1">
+          <div className="h-full w-full">
+            <div className="w-full h-full pb-10 py-3 rounded-tl-100 rounded-tr-100 bg-project-2">
+              <div className="my-16 ml-16">
+                <h1 className="text-5xl font-medium">FEB UMJ E-SDM</h1>
               </div>
-            </div> */}
+              <div className="flex flex-row w-full justify-center">
+                <div className="flex flex-row w-full gap-2 px-8 justify-center">
+                  <div className="flex flex-col gap-2">
+                    <Image
+                      src="https://res.cloudinary.com/iqbalululazmi/image/upload/v1629902210/esdm-home_o2dcai.png"
+                      width="545"
+                      height="290"
+                      alt="esdm home desktop"
+                    />
+                    <Image
+                      src="https://res.cloudinary.com/iqbalululazmi/image/upload/v1629902208/esdm-struktural_pmnxqt.png"
+                      width="545"
+                      height="290"
+                      alt="esdm home desktop"
+                    />
+                  </div>
+                  <div>
+                    <Image
+                      src="https://res.cloudinary.com/iqbalululazmi/image/upload/v1629902209/esdm-mobile-home_m4mq3v.png"
+                      width="350"
+                      height="700"
+                      alt="esdm home"
+                    />
+                  </div>
+                  <div>
+                    <Image
+                      src="https://res.cloudinary.com/iqbalululazmi/image/upload/v1629902210/esdm-mobile-struktural_fwehjz.png"
+                      width="350"
+                      height="700"
+                      alt="esdm payment"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="portofolio" className="h-full bg-project-2">
+          <div className="h-full w-full">
+            <div className="w-full h-full pb-10 py-3 rounded-tl-100 rounded-tr-100 bg-project-3">
+              <div className="my-16 ml-16">
+                <h1 className="text-5xl font-medium text-white-young">Studi</h1>
+              </div>
+              <div className="flex flex-row w-full justify-center">
+                <div className="flex flex-row w-full gap-2 px-8 justify-center">
+                  <div className="mt-16">
+                    <Image
+                      src="https://res.cloudinary.com/iqbalululazmi/image/upload/v1629902209/studi-classroom_skjgay.png"
+                      width="350"
+                      height="755"
+                      alt="studi"
+                    />
+                  </div>
+                  <div>
+                    <Image
+                      src="https://res.cloudinary.com/iqbalululazmi/image/upload/v1629902208/studi-home_er5tc8.png"
+                      width="350"
+                      height="755"
+                      alt="studi"
+                    />
+                  </div>
+                  <div className="mt-16">
+                    <Image
+                      src="https://res.cloudinary.com/iqbalululazmi/image/upload/v1629902208/studi-statistic_jtfjco.png"
+                      width="350"
+                      height="755"
+                      alt="studi"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
       </Container>
