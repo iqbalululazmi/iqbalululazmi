4b965f4ae128f03646144084fb9a8f7cfbdc474e
iqbalululazmi <iqbalululazmi@gmail.com>
2021-08-26 22:01:20 +0700

    feat: create scroll button

---
+++ src/components/styled/Button.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const ButtonToScroll = styled.div`
+  position: fixed;
+  width: 50px;
+  bottom: 40px;
+  right: 20px;
+  height: 50px;
+  font-size: 3rem;
+  z-index: 1;
+  cursor: pointer;
+  color: #5a88ff;
+`
