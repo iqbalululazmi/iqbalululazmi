f6d26519a4c019c90df804809ec116131e929a31
iqbalululazmi <iqbalululazmi@gmail.com>
2021-08-27 22:11:08 +0700

    feat: add framer motion

--- src/components/core/Layout.tsx
+++ src/components/core/Layout.tsx
@@ -3,15 +3,28 @@ import { Header } from './Header'
 import { MetaHead } from './MetaHead'
 import { Container } from '@components/core/Container'
 import ScrollButton from '@components/ScrollButton'
+import { motion } from 'framer-motion'
 
 export const Layout = ({ children, classLayout, classContainer, isNavbarSecond }: any) => {
+  const variants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: 0.2,
+      },
+    },
+  }
+
   return (
     <div className={classLayout}>
       <MetaHead />
       <Header isNavbarSecond={isNavbarSecond} />
-      <Container className={classContainer}>
-        <div className="max-w-full mx-auto px-8 py-10 sm:px-6 sm:py-2">{children}</div>
-      </Container>
+      <motion.main variants={variants} initial="hidden" animate="visible">
+        <Container className={classContainer}>
+          <div className="max-w-full mx-auto px-8 py-10 sm:px-6 sm:py-2">{children}</div>
+        </Container>
+      </motion.main>
       <Footer />
       <ScrollButton />
     </div>
